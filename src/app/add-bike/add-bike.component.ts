28bb344bcffdb30c518efeac53cec96fce7d20d3
kawther <kawther.bjaoui@gmail.com>
2020-05-16 19:12:15 +0100

    Date/timer picker

--- src/app/add-bike/add-bike.component.ts
+++ src/app/add-bike/add-bike.component.ts
@@ -6,7 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-bike.component.scss']
 })
 export class AddBikeComponent implements OnInit {
-
+  public selectedMoments = [
+    new Date(2018, 1, 12, 10, 30),
+    new Date(2018, 3, 21, 20, 30)
+];
   constructor() { }
 
   ngOnInit() {
--- src/app/add-bike/add-bike.module.ts
+++ src/app/add-bike/add-bike.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { AddBikeRoutingModule } from './add-bike-routing.module';
 import {AddBikeComponent} from './add-bike.component';
-
+import { OwlDateTimeModule, OwlNativeDateTimeModule } from 'ng-pick-datetime';
 
 @NgModule({
   declarations: [AddBikeComponent],
   imports: [
     CommonModule,
-    AddBikeRoutingModule
+    AddBikeRoutingModule,
+    OwlDateTimeModule,
+     OwlNativeDateTimeModule
   ]
 })
 export class AddBikeModule { }
