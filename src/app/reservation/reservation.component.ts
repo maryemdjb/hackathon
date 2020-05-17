b828ebcbebf7ef51d5eb56a54673de853686f05f
Djebbi Maryem <maryem.djebbi@sofrecom.com>
2020-05-17 05:44:17 +0200

    4 ysf

--- src/app/reservation/reservation.component.ts
+++ src/app/reservation/reservation.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {FormBuilder} from '@angular/forms';
 import {Router} from '@angular/router';
 import {ReservationService} from './reservation.service';
@@ -14,51 +14,53 @@ export class ReservationComponent implements OnInit {
   public selectedMoments = [
     new Date(2018, 1, 12, 10, 30),
     new Date(2018, 3, 21, 20, 30)
-];
-isLinear = false;
+  ];
+  isLinear = false;
   startDate: any;
   endDate: any;
 
-  constructor(private _formBuilder: FormBuilder,
-              private reservationService : ReservationService,
-              private router : Router ) { }
+  constructor(
+              private formBuilder: FormBuilder,
+              private reservationService: ReservationService,
+              private router: Router) {
+  }
 
   ngOnInit() {
 
   }
 
 
-/*  reserve() {
-    console.log("coucouuuuuuuuuuuuuuu");
-    console.log(
-    String(this.model.debut));
-    this.reservationService.reserveForm(String(this.model.debut), String(this.model.fin), 1,1)
-      .subscribe(response => {
-        console.log("hhhhhhhhhh");
+  /*  reserve() {
+      console.log("coucouuuuuuuuuuuuuuu");
+      console.log(
+      String(this.model.debut));
+      this.reservationService.reserveForm(String(this.model.debut), String(this.model.fin), 1,1)
+        .subscribe(response => {
+          console.log("hhhhhhhhhh");
 
-        alert("Votre inscription est effectuée avec succès")
+          alert("Votre inscription est effectuée avec succès")
 
 
-         this.router.navigate(['/home']);
-          // console.log(this.model.username, this.model.password);
+           this.router.navigate(['/home']);
+            // console.log(this.model.username, this.model.password);
 
-      }, error => {
-        console.error(error);
-      });
-     this.sendEmail()
-  }*/
+        }, error => {
+          console.error(error);
+        });
+       this.sendEmail()
+    }*/
 
-/*  async sendEmail() {
-    let receiver ='kiki';
-    let nom ='kawther';
-    let mail ='kawther.bjaoui@gmail.com'
+  /*  async sendEmail() {
+      let receiver ='kiki';
+      let nom ='kawther';
+      let mail ='kawther.bjaoui@gmail.com'
 
-    try {
-      const data = await this.reservationService.notifEmail(receiver,nom,mail);
-      // Since sendEmail again returns a Promise, I can await it.
-      console.log(data);
-    } catch (error) {
-      console.log(error);
-    }
-  }*/
+      try {
+        const data = await this.reservationService.notifEmail(receiver,nom,mail);
+        // Since sendEmail again returns a Promise, I can await it.
+        console.log(data);
+      } catch (error) {
+        console.log(error);
+      }
+    }*/
 }
